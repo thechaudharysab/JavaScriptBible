5ab1eb5236a3234800f1d597c09b9945cdee2728
Chaudhry Talha <gameral@live.com>
2020-09-29 12:00:12 +0800

    Basic operators finished

--- JavaScript/2. JavaScript Basics/script.js
+++ JavaScript/2. JavaScript Basics/script.js
@@ -1,1 +1,38 @@
-console.log('Hello World!!!');
+//****Let's start coding****
+//console.log('Hello World!!!');
+
+//****Variables & Data Types****
+//var firstName = 'Talha';
+//var lastName = 'Chaudhry';
+//var age = 28;
+//var isOfValidAge = true;
+
+//console.log(isOfValidAge);
+
+//var job;
+//console.log(job);
+//
+//job = 'Teacher';
+//console.log(job);
+
+//****Basic Operators****
+
+//var yearUserWasBornIn = 2020 - 28; //Current year minus age of person
+//console.log(yearUserWasBornIn);
+
+var currentYear = 2020;
+var user1Age = 29;
+var user2Age = 28;
+//var yearUserWasBornIn = currentYear - user1Age;
+//console.log(yearUserWasBornIn);
+//
+//console.log(currentYear + 2);
+//console.log(currentYear * 2);
+//console.log(currentYear / 2);
+
+//logical operator
+var isUser1Older = user1Age > user2Age;
+//console.log(isUser1Older);
+
+//typeof operator
+console.log(typeof isUser1Older);
