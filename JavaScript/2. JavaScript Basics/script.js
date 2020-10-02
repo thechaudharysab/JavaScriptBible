073a16e7993cd043063406d9b3066608598a0fef
Chaudhry Talha <gameral@live.com>
2020-10-02 15:55:46 +0800

    coding challenge 2 started

--- JavaScript/2. JavaScript Basics/script.js
+++ JavaScript/2. JavaScript Basics/script.js
@@ -57,7 +57,7 @@
 //console.log(x, y);
 
 
-//****CODING CHALLENGE 1 SOLUTION****
+//**** CODING CHALLENGE 1 SOLUTION ****
 ////1
 //var markHeight = 1.85928; //in meters
 //var markMass = 70; //in KG
@@ -113,3 +113,101 @@
 //}
 
 //****Boolean Logic****
+//var firstName = 'John';
+//var age = 16;
+//
+//if(age < 13) {
+//    console.log(firstName + ' is a boy.');
+//} else if(age >= 13 && age < 20) {
+//    console.log(firstName + ' is a teenager.');
+//} else if (age >=20 && age < 30) {
+//    console.log(firstName + ' is a young man.')       
+//} else {
+//    console.log(firstName + ' is a man.');
+//}
+
+//****The Ternary Operator and Switch Statements****
+
+//ternary Operator
+//var firstName = 'John';
+//var age = 16;
+//
+//age >= 18 ? console.log(firstName + ' drinks beer') : console.log(firstName + ' drinks juice');
+           
+//var age = 22;
+//var drink = age >= 18 ? 'beer' : 'juice';
+//console.log(drink);
+
+//Switch statement
+//var job = 'teacher';
+//switch(job) {
+//    case 'teacher':
+//    case 'instructor':
+//        console.log('John teach kids how to code');
+//        break;
+//    case 'driver':
+//        console.log('John drives GrabCar in Kuala Lumpur');
+//        break;
+//    case 'designer':
+//        console.log('John design awesome websites');
+//        break;
+//    default:
+//        console.log('John does something else');
+//}
+
+//var age=16;
+//switch(true) {
+//    case age < 13:
+//        console.log('John is a boy');
+//        break;
+//    case age >= 13 && age < 20:
+//        console.log('John is a teenager');
+//        break;
+//    case age >= 20 && age < 30:
+//        console.log('John is a young man');
+//        break;
+//    default:
+//        console.log('John is a man');
+//}
+
+//****Truthy and Falsy Values & Equality Operators****
+
+//var height;
+//height = '';
+//if(height || height === 0) {
+//    console.log('Variable is defined');
+//} else {
+//    console.log('Variable has not been defined');
+//}
+
+//var height = 23;
+//
+//if(height == '23') {
+//    console.log('The == operator does type coercion.');
+//}
+
+//**** Coding Challenge 2 Solution ****
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
